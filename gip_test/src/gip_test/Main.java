4071c17baf7149b0bb9d98cf78d7e7664cff2788
big2-15 <big2-15@big2-15-PC>
2018-07-10 12:11:16 +0900

    COMMIIT

--- gip_test/src/gip_test/Main.java
+++ gip_test/src/gip_test/Main.java
@@ -2,7 +2,11 @@ package gip_test;
 
 public class Main {
 	public static void main(String args[]) { 
+		System.out.println("Bye Bye");
+	}
+	public static void main(String args[]) { 
 		System.out.println("Hello java");
 	}
-	
 }
+
+

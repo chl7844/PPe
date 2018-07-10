cd66d43e86a452ab7526102163b8b55398b88481
big2-15 <big2-15@big2-15-PC>
2018-07-10 12:29:42 +0900

    Main java()

--- gip_test/src/gip_test/Main.java
+++ gip_test/src/gip_test/Main.java
@@ -4,8 +4,9 @@ public class Main {
 	
 	public static void main(String args[]) { 
 		System.out.println("Bye Bye");
-	}
-	public static void main(String args[]) { 
+		System.out.println("hello");
+	
+	
 		System.out.println("Hello java");
 	}
 }
